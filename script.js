6fe86124cb70142ce9c5396537111173c97eeb98
M1N00 <antoni.debaz@gmail.com>
2025-04-02 17:46:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//C'est le truc que Loris a demandé d'ajouter mais c'est franchement pas ouf
+
+/*
+const container = document.querySelector('.Chat_vente-container');
+
+container.addEventListener('scroll', () => {
+    if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+            container.scrollTo({ left: 0, behavior: 'smooth' });
+    }
+});
+
+*/
+const AproposButton = document.getElementById('APROPOS');
+
+AproposButton.addEventListener('click', () => {
+    window.location.href = 'Pages/Apropos.html';
+});
